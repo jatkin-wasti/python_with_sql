b6790344a3e2a0e50670507b18f4be60d3c73eb6
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-11 16:28:13 +0000

    completed initial task with comments included, created new file for SQL OOP task

--- pyodbc_task.py
+++ pyodbc_task.py
@@ -1,19 +1,23 @@
+# Importing pyodbc so that we can connect to our SQL database
 import pyodbc
 
 
 class PyodbcTask:
+    # Method that will connect us to the northwind database
     def connect(self):
         # We will connect to our Northwind DB which we've already used in the SQL week
         server = "databases1.spartaglobal.academy"
         database = "Northwind"
-        username = "**"
-        password = "********"
+        username = "SA"
+        password = "Passw0rd2018"
         # Server name, database name, username, and password are required to connect to pyodbc
         northwind_connection = pyodbc.connect(('DRIVER={ODBC Driver 17 for SQL Server};SERVER='
                                                + server + ';DATABASE=' + database + ';UID='
                                                + username + ';PWD=' + password))
-        return northwind_connection
+        cursor = northwind_connection.cursor()
+        return cursor
 
+    # Method that will take any number of arguments and will try to create a table using them
     def create_table(self, *args):
         i = 0
         sql_query = "CREATE TABLE"
@@ -28,16 +32,32 @@ class PyodbcTask:
                 sql_query = sql_query + _ + ','
                 i += 1
 
+        # Stripping the final comma and ending the query with an ending parenthesis and semicolon
         sql_query = sql_query.rstrip(sql_query[-1]) + ');'
+        # Creating our cursor and running the query we just built
         cursor = self.connect()
         cursor.execute(sql_query)
 
-    def add_to_table(self, table_name):
-        pass
-        # cursor = self.northwind_connection.cursor()
-        # cursor.execute(f"INSERT INTO {table_name} ()")
+    # Method that will insert provided values into the provided table, this will only work for the Jamie_Table
+    def insert(self):
+        # Creating our cursor and creating our table as it seems that the table must be made in the same function as
+        # the one that inserts into it (this may be a db issue)
+        cursor = self.connect()
+        cursor.execute("CREATE TABLE Jamie_Table (FirstName varchar(30), LastName varchar(30), Age int);")
+        # Retrieving user input to add to a table
+        table_name = input("Please enter the name of the table you wish to insert data into:  ")
+        val1 = input("Please enter the value you'd like to input into the first column:  ")
+        val2 = input("Please enter the value you'd like to input into the second column:  ")
+        val3 = input("Please enter the value you'd like to input into the third column:  ")
+        # Executing the query with the users inputs
+        cursor.execute(f"INSERT INTO {table_name} (FirstName, LastName, Age)\n"
+                       f"VALUES ('{val1}', '{val2}', {val3});")
 
 
+# Creating an instance of our class to run its functionality
 test = PyodbcTask()
+# Testing our creating tables function
 test.create_table("Jamie_Table", "FirstName", "varchar(30)", "LastName", "varchar(30)",
                                                                          "Age", "int")
+# Testing our insert to tables function
+test.insert()
